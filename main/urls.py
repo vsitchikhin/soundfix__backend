55c8b9b6f8cd1c9184418fdd1cd18ba5c8723fd6
Vladimir Sitchikhin <vsitchikhin@gmail.com>
2022-06-07 19:48:14 +0300

    LOG-12 [feature] - удалил приложения main и users, т.к. main - изначально неправильная реализация, а users нет необходимости реализовывать, в силу наличия встроенного в django функционала авторизации и регистрации

--- main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', index, name='main'),
-    path('signup/', signup, name='signip'),
-    path('login/', login, name='login'),
-]
--- soundfix__backend/urls.py
+++ soundfix__backend/urls.py
@@ -13,13 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from users.views import *
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/user', UserAPIView.as_view())
+
 ]
